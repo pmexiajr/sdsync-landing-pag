1d472b2d03eb1de63ea958a449cb6307b3f3b8c9
unknown <devtaveira@gmail.com>
2024-04-18 16:50:53 -0300

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,23 @@ import logo from "../../public/SDSync - Logo.png";
 export default function Home() {
   return (
     <main className="w-full">
-      <nav className="primaria border p-4">Início</nav>
+      <nav className="primaria border p-4 text-white">Início</nav>
       <section className="bg-white my-10 flex justify-center">
         <Image src={logo} width={550} height={550} alt="SDSyncLogo" />
       </section>
-      <section className="secundaria p-20 flex justify-evenly">
+      <section className="secundaria p-20 flex justify-evenly items-center">
         <div>Bem Vindo!</div>
-        <div>
+        <div className="text-center flex flex-col gap-4">
+          <h3>ACESSE NOSSA PLATAFORMA: </h3>
           <a href="http://app.sdsync.com.br" className="font-bold">
-            APP SDSYNC
+            <h1 className="text-5xl border-4 text-white border-white rounded-lg p-3">
+              APP SDSYNC
+            </h1>
           </a>
         </div>
+      </section>
+      <section className="primaria text-white p-20 flex justify-evenly">
+        <div>Bem Vindo!</div>
       </section>
       <footer className="bg-gray-300 p-20 flex justify-center">@mextech</footer>
     </main>
